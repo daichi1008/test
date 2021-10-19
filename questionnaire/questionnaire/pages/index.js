e07972c5f65971285e01cb2453e2230394762271
daichi1008 <forester.dn1008@gmail.com>
2021-10-19 21:32:48 +0900

    1019

--- questionnaire/questionnaire/pages/index.js
+++ questionnaire/questionnaire/pages/index.js
@@ -1,91 +1,119 @@
-import { useForm } from "react-hook-form";
-import Container from '@material-ui/core/Container';
+import { useForm, Controller } from "react-hook-form";
+import Container from "@material-ui/core/Container";
+import Input from "@material-ui/core/Input"
 export default function Home() {
-  const { register, handleSubmit, formState: { errors } } = useForm()
+  const { register, handleSubmit, watch, formState: { errors }, control } = useForm()
 
   const onSubmit = (data) => {
     console.log(data)
+
   }
+  console.log(watch("isLearning"))
+  console.log(watch("wasLearning"))
   return (
     <>
-      {/* <Container> */}
-      <h1>プログラミング学習に関するアンケート</h1>
+      <Container>
+        <h1>プログラミング学習に関するアンケート</h1>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label htmlFor="name">Q1.名前を入力してください（匿名可）。</label>
-          <input id="name" {...register("name")} />
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label htmlFor="name">Q1.名前を入力してください（匿名可）。</label>
+            <Controller
+              name="name"
+              defaultValue=""
+              control={control}
+              render={({ field: { value, onChange } }) => <Input value={value} onChange={onChange} />}
+            />
+          </div>
 
-        <div>
-          <label htmlFor="birth">Q2. 生年月日を入力してください。(例： 19900101)</label>
+          <div>
+            <label htmlFor="birth">Q2. 生年月日を入力してください。(例： 19900101)</label>
+            <Controller
+              name="birth"
+              defaultValue=""
+              control={control}
+              rules={{ required: true, pattern: /^[0-9]{8}$/ }}
+              render={({ field: { value, onChange } }) => <Input value={value} onChange={onChange} />}
+            />
+            {
+              errors.birth && errors.birth.type === "required" ?
+                <span>このフィールドは回答必須です。</span> : null
+            }
+            {
+              errors.birth && errors.birth.type === "pattern" ?
+                <span>整数8桁で入力してください。</span> : null
+            }
+          </div>
 
-          <input id="birth" {...register("birth", { required: true, pattern: /^[0-9]{8}$/ })} />
-          {
-            errors.birth && errors.birth.type === "required" ?
-              <span>このフィールドは回答必須です。</span> : null
-          }
-          {
-            errors.birth && errors.birth.type === "pattern" ?
-              <span>整数8桁で入力してください。</span> : null
-          }
-        </div>
+          <div>
+            <span>Q3. 現在、プログラミングを学習していますか？</span>
+            <input
+              id="isLearning1"
+              defaultValue="true" {...register("isLearning", { required: true })}
+              name="isLearning"
+              type="radio"
+            />
+            <label htmlFor="isLearning1">はい</label>
 
-        <div>
-          <span>Q3. 現在、プログラミングを学習していますか？</span>
-          <input
-            id="isLearning1"
-            {...register("isLearning", { required: true })}
-            name="isLearning"
-            type="radio"
-            value="true"
-          />
-          <label htmlFor="isLearning1">はい</label>
+            <input
+              id="isLearning2"
+              defaultValue="false"{...register("isLearning", { required: true })}
+              name="isLearning"
+              type="radio"
+            />
+            <label htmlFor="isLearning2">いいえ</label>
 
-          <input
-            id="isLearning2"
-            {...register("isLearning", { required: true })}
-            name="isLearning"
-            type="radio"
-            value="false"
-          />
-          <label htmlFor="isLearning2">いいえ</label>
+            {
+              errors.isLearning &&
+              <span>このフィールドは回答必須です。</span>
+            }
+          </div>
 
-          {
-            errors.isLearning &&
-            <span>このフィールドは回答必須です。</span>
-          }
-        </div>
+          <div>
+            <span>Q4. これまでに、プログラミングを学習したことがありますか？</span>
+            <input
+              id="wasLearning1"
+              defaultValue="true"{...register("wasLearning", { required: true })}
+              name="wasLearning"
+              type="radio"
 
-        <div>
-          <span>Q4. これまでに、プログラミングを学習したことがありますか？</span>
-          <input
-            id="wasLearning1"
-            {...register("wasLearning", { required: true })}
-            name="wasLearning"
-            type="radio"
-            value="true"
-          />
-          <label htmlFor="wasLearning1">はい</label>
+            />
+            <label htmlFor="wasLearning1">はい</label>
 
-          <input
-            id="wasLearning2"
-            {...register("wasLearning", { required: true })}
-            name="wasLearning"
-            type="radio"
-            value="false"
-          />
-          <label htmlFor="wasLearning2">いいえ</label>
+            <input
+              id="wasLearning2"
+              defaultValue="false"{...register("wasLearning", { required: true })}
+              name="wasLearning"
+              type="radio"
 
-          {
-            errors.wasLearning &&
-            <span>このフィールドは回答必須です。</span>
-          }
-        </div>
+            />
+            <label htmlFor="wasLearning2">いいえ</label>
 
-        <input type="submit" value="アンケートを提出する" />
-      </form>
-      {/* </Container> */}
+            {
+              errors.wasLearning &&
+              <span>このフィールドは回答必須です。</span>
+            }
+          </div>
+          <div>
+            {
+              watch("isLearning") === "true" || watch("wasLearning") === "true" ?
+                <label htmlFor="programLanguage">Q5.今まで学習したことのあるプログラミング言語をすべて教えてください。</label> : null
+            } {
+              watch("isLearning") === "true" || watch("wasLearning") === "true" ?
+                <Controller
+                  name="programLanguage"
+                  defaultValue=""
+                  control={control}
+                  render={({ field: { value, onChange } }) => <Input value={value} onChange={onChange} />}
+                /> : null
+
+            }
+
+          </div>
+
+          <input type="submit" value="アンケートを提出する" />
+        </form>
+      </Container>
     </>
   )
 }
